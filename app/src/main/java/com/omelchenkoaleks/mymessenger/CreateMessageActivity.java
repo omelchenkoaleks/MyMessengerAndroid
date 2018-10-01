31945b3dfbefb70c79fcb8b5bc6d092942ee0ada
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-01 08:48:46 +0300

    Используем Intent для запуска второй активности.

--- app/src/main/java/com/omelchenkoaleks/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/omelchenkoaleks/mymessenger/CreateMessageActivity.java
@@ -1,6 +1,7 @@
 package com.omelchenkoaleks.mymessenger;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,6 +15,8 @@ public class CreateMessageActivity extends Activity {
 
     // метод вызовется при клике на кнопке send
     public void onSendMessage(View view) {
-
+        // используем Интент, чтобы запустить вторую активность
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        startActivity(intent);
     }
 }
