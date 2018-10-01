86896bda204600ee2fcc2cbb1cce2e1a1ffe8e34
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-01 09:21:29 +0300

    Во второй активности получаем intent и выводим сообщение пришедшее с ним в TextView.

--- app/src/main/java/com/omelchenkoaleks/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/omelchenkoaleks/mymessenger/CreateMessageActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends Activity {
 
@@ -15,8 +16,14 @@ public class CreateMessageActivity extends Activity {
 
     // метод вызовется при клике на кнопке send
     public void onSendMessage(View view) {
+        EditText messageView = findViewById(R.id.message);
+        // получаем текст из текстового поля
+        String messageText = messageView.getText().toString();
+
         // используем Интент, чтобы запустить вторую активность
         Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        // добавляем текст в интент под именем "message"
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/omelchenkoaleks/mymessenger/ReceiveMessageActivity.java
+++ app/src/main/java/com/omelchenkoaleks/mymessenger/ReceiveMessageActivity.java
@@ -1,13 +1,24 @@
 package com.omelchenkoaleks.mymessenger;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReceiveMessageActivity extends Activity {
+    public static final String EXTRA_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
+
+        // получаем Интент
+        Intent intent = getIntent();
+        // извлекаем из него сообщение методом getStringExtra()
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+
+        TextView messageView = findViewById(R.id.message);
+        messageView.setText(messageText);
     }
 }
