37a0b8277e13252b7d702d072012d1177e4e2601
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-01 08:27:58 +0300

    Создан макет, строковые ресурсы и пустой пока метод для обработки нажатия на кнопку.

--- app/src/main/java/com/omelchenkoaleks/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/omelchenkoaleks/mymessenger/CreateMessageActivity.java
@@ -2,6 +2,7 @@ package com.omelchenkoaleks.mymessenger;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateMessageActivity extends Activity {
 
@@ -9,5 +10,10 @@ public class CreateMessageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
+    }
+
+    // метод вызовется при клике на кнопке send
+    public void onSendMessage(View view) {
+
     }
 }
